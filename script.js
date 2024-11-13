75341cf85db5c59413aca46700639396d0b67843
Alejandro Arco <alejandroarpu@gmail.com>
2024-11-13 21:26:40 +0100

    feat: add script to play with buttons

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const options = ["rock", "paper", "scissors"];
 
+let resultDiv = document.querySelector(".results");
+
 let humanScore = 0;
 let computerScore = 0;
 
@@ -7,52 +9,94 @@ function getComputerChoice() {
   return options[Math.floor(Math.random() * options.length)];
 }
 
-function getHumanChoice() {
-  return prompt("Hey human! Let's play rock paper scissors.").toLowerCase();
+function checkTheWinner(humanScore, computerScore) {
+  return humanScore > computerScore
+    ? "✨ Sensational! You won the game! ✨"
+    : "😔 Oh no! You lost! 😔";
 }
 
+// function getHumanChoice() {
+//   return prompt("Hey human! Let's play rock paper scissors.").toLowerCase();
+// }
+
 function playRound(humanChoice, computerChoice) {
+  const pRound = document.createElement("p");
+  pRound.classList.add("round");
+  pRound.setAttribute("style", "font-weight: bold");
   if (humanChoice === "rock") {
     if (computerChoice === "paper") {
-      console.log("You lose! Paper beats rocks.");
+      pRound.textContent = "You lose! Paper beats rocks.";
+      pRound.style.color = "red";
       computerScore++;
     } else if (computerChoice === "scissors") {
-      console.log("You win! Rock beats scissors");
+      pRound.textContent = "You win! Rock beats scissors.";
+      pRound.style.color = "green";
       humanScore++;
     } else {
-      console.log("No body wins. Rock draws.");
+      pRound.textContent = "No body wins. Rock draws.";
+      pRound.style.color = "orange";
     }
   } else if (humanChoice === "scissors") {
     if (computerChoice === "paper") {
-      console.log("You win! Scissors beats paper.");
+      pRound.textContent = "You win! Scissors beats paper.";
+      pRound.style.color = "green";
       humanScore++;
     } else if (computerChoice === "rock") {
-      console.log("You lose! Rock beats scissors");
+      pRound.textContent = "You lose! Rock beats scissors.";
+      pRound.style.color = "red";
       computerScore++;
     } else {
-      console.log("No body wins. Scissor draws.");
+      pRound.textContent = "No body wins. Scissor draws.";
+      pRound.style.color = "orange";
     }
   } else {
     if (computerChoice === "scissors") {
-      console.log("You lose! Scissors beats paper.");
+      pRound.textContent = "You lose! Scissors beats paper.";
+      pRound.style.color = "red";
       computerScore++;
     } else if (computerChoice === "rock") {
-      console.log("You win! Paper beats rock");
+      pRound.textContent = "You win! Paper beats rock.";
+      pRound.style.color = "green";
       humanScore++;
     } else {
-      console.log("No body wins. Paper draws.");
+      pRound.textContent = "No body wins. Paper draws.";
+      pRound.style.color = "orange";
     }
   }
+  resultDiv.appendChild(pRound);
 }
 
-function playGame() {
-  for (let i = 1; i <= 5; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
+let optionsDiv = document.querySelector(".options");
+optionsDiv.addEventListener("click", (e) => {
+  const existingWinner = document.querySelector(".winner");
+  if (existingWinner) existingWinner.remove();
+
+  const computerSelection = getComputerChoice();
+
+  const humanChoiceWithoutEmoji = e.target.textContent.includes("paper")
+    ? e.target.textContent.slice(0, -3)
+    : e.target.textContent.slice(0, -2);
+  playRound(humanChoiceWithoutEmoji, computerSelection);
+  if (humanScore == 5 || computerScore == 5) {
+    document.querySelectorAll(".round").forEach((el) => el.remove());
+    const winner = checkTheWinner(humanScore, computerScore);
+    const pWinner = document.createElement("p");
+    pWinner.classList.add("winner");
+    pWinner.textContent = winner;
+    resultDiv.appendChild(pWinner);
+    humanScore = 0;
+    computerScore = 0;
   }
-  if (humanScore >= computerScore) console.log("Human wins!");
-  else console.log("Computer wins!");
-}
+});
 
-playGame();
+// function playGame() {
+//   for (let i = 1; i <= 5; i++) {
+//     const humanSelection = getHumanChoice();
+//     const computerSelection = getComputerChoice();
+//     playRound(humanSelection, computerSelection);
+//   }
+//   if (humanScore >= computerScore) console.log("Human wins!");
+//   else console.log("Computer wins!");
+// }
+
+// playGame();
